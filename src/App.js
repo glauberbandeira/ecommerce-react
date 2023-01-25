4e221ac02092c7bacdabe1f07f9fe36211f7e1ef
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-01-26 01:06:18 +1100

    commit creating overall structure - part 1

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useSelector } from 'react-redux';
+
+import { Container, Menu, PageBody } from './AppStyled';
+
+import HomeScreen from './pages/HomeScreen';
+import Tela2Screen from './pages/Tela2Screen';
+
+export default () => {
+    const name = useSelector(state => state.user.name);
+
+    return (
+        <BrowserRouter>
+        <Container>
+            <Menu>
+                
+            </Menu>
+            <PageBody>
+                <Switch>
+                    <Route exact path="/">
+                        <HomeScreen />
+                    </Route>
+                    <Route path="/tela2/:nome">
+                        <Tela2Screen />
+                    </Route>
+                </Switch>
+            </PageBody>
+        </Container>
+        </BrowserRouter>
+    );
+}
---
+++ src/AppStyled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+display: flex;
+height: 100vh;
+background-color: #EEEEEE;
+`;
+
+export const Menu = styled.div`
+display: flex;
+background-color: #000000;
+width: 100px;
+`;
+
+export const PageBody = styled.div`
+display: flex;
+background-color: #EFEFEF;
+flex: 1;
+`;
