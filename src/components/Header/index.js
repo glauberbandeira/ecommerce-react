531fd72488584b6afd5ec4e3f5fea9968afa1933
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-01-28 16:38:08 +1100

    commit home part - 2 seach

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Container, Logo, SearchInput } from './styled';
 
 export default () => {
+    const [inputActive, setInputActive] = useState(false);
+
+    const handleInputFocus = () => {
+        setInputActive(true)
+    }
+
+    const handleInputBlur = () => {
+        setInputActive(false)
+    }
+
+
+
     return (
         <Container>
             <Logo src="/assests/logo.png"/>
-            <SearchInput type="text" placeholder="Digite um produto..." />
+            <SearchInput 
+            type="text" 
+            placeholder="Digite um produto..."
+            active={inputActive}
+            onFocus={handleInputFocus} 
+            onBlur={handleInputBlur}
+            />
         </Container>
     );
 }
