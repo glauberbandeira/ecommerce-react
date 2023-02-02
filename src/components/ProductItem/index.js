80943d4822eb45209fe959f18eeb84405afa3b90
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-02-02 22:46:24 +1100

    display products part - 2

--- src/components/ProductItem/index.js
+++ src/components/ProductItem/index.js
@@ -1,10 +1,30 @@
 import React from "react";
-import { Container } from "./styled";
+import { 
+    Container,
+    ProductPhotoArea,
+    ProductInfoArea,
+    ProductButtonArea,
+    ProductPhoto,
+    ProductPrice,
+    ProductName,
+    ProductIngredients,
+    ProductButton
+} from "./styled";
 
-export default () => {
+export default ({data}) => {
     return (
         <Container>
-            
+            <ProductPhotoArea>
+                <ProductPhoto src={data.image} />
+            </ProductPhotoArea>
+            <ProductInfoArea>
+                <ProductName>{data.name}</ProductName>
+                <ProductPrice>{data.price}</ProductPrice>
+                <ProductIngredients>{data.ingredients}</ProductIngredients>
+            </ProductInfoArea>
+            <ProductButtonArea>
+                <ProductButton src="/assests/next.png" />
+            </ProductButtonArea>
         </Container>
     )
 }
